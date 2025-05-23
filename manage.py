dba82e455aa43e11bba51f16a08bf1c994a3b650
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2025-05-23 21:01:06 +0100

    manage admin

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.contrib.auth import get_user_model
 
 
 def main():
@@ -18,5 +19,19 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+    User = get_user_model()
+    if os.getenv("CREATE_SUPERUSER") == "true":
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            print("Superuser created.")
+        else:
+            print("Superuser already exists.")
+
+
 if __name__ == '__main__':
     main()
+
+    
